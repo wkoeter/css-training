fd0d33d9a89e6ecbdf3b87537f0b3198183fcdcb
wkoeter <willemkoeter@outlook.com>
2021-04-28 21:08:33 +0200

    4 files modified, 1 added, 1 deleted

---
+++ shared.js
@@ -0,0 +1,49 @@
+var modal = document.querySelector(".modal");
+var backdrop = document.querySelector(".backdrop");
+var toggleButton = document.querySelector(".toggle-button");
+var selectPlanButton = document.querySelectorAll(".plan button");
+var mobileNav = document.querySelector(".mobile-nav");
+var modalButtons = document.querySelectorAll(".modal button");
+
+var displayBlock = "block";
+var displayNone = "none";
+
+toggleButton.addEventListener("click", function() {
+  open(backdrop);
+  open(mobileNav);
+});
+
+selectPlanButton.forEach(b => {
+  b.addEventListener("click", function() {
+    showModal();
+  });
+});
+
+modalButtons.forEach(modalButton => {
+  modalButton.addEventListener("click", closeModal);
+});
+
+backdrop.addEventListener("click", function() {
+  close(mobileNav);
+  closeModal();
+});
+
+function closeModal() {
+  if(modal){
+close(modal);
+  }
+  close(backdrop);
+}
+
+function showModal() {
+  open(modal);
+  open(backdrop);
+}
+
+function open(item) {
+  item.classList.add("open");
+}
+
+function close(item) {
+  item.classList.remove("open");
+}
